33a6dc3c6e1aff26b0431a182497f14f69d57f35
Tulis12 <agma116434@gmail.com>
2026-07-16 20:36:58 +0200

    add: proxie command

--- src/main/java/dev/tulis/proxieSuite/Administration/ProxieSuiteCommand.java
+++ src/main/java/dev/tulis/proxieSuite/Administration/ProxieSuiteCommand.java
@@ -1,16 +1,20 @@
 package dev.tulis.proxieSuite.Administration;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.command.CommandSource;
-import com.velocitypowered.api.command.SimpleCommand;
-import dev.tulis.proxieSuite.CommandUtils.CommandPermission;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.proxy.ProxyServer;
 import dev.tulis.proxieSuite.Main.Main;
 import dev.tulis.proxieSuite.i18n.I18N;
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 
-public final class ProxieSuiteCommand implements SimpleCommand {
+public final class ProxieSuiteCommand {
 
     private Main plugin;
 
@@ -24,64 +28,80 @@ public final class ProxieSuiteCommand implements SimpleCommand {
             .plugin(plugin)
             .build();
 
-        commandManager.register(commandMeta, this);
-    }
-
-    @Override
-    public void execute(Invocation invocation) {
-        CommandSource source = invocation.source();
-        String[] args = invocation.arguments();
-
-        if (!CommandPermission.handlePermission("proxiesuite", invocation)) {
-            return;
-        }
-
-        if (args.length == 0) {
-            source.sendMessage(I18N.commandSyntax("proxiesuite", invocation));
-            return;
-        }
-
-        switch (args[0]) {
-            case "reload": {
-                try {
-                    boolean success = plugin.getConfig().reload();
-
-                    if (success) {
-                        I18N.sendMessage(
-                            source,
-                            "command.success.proxiesuite.reload"
-                        );
-
-                        return;
-                    }
-                } catch (IOException e) {
-                    plugin.getLogger().error("Unable to reload config!", e);
-                }
-
-                I18N.sendMessage(source, "command.error.proxiesuite.reload");
-
-                break;
-            }
-            default: {
-                source.sendMessage(
-                    I18N.commandSyntax("proxiesuite", invocation)
-                );
-            }
-        }
-    }
-
-    @Override
-    public boolean hasPermission(Invocation invocation) {
-        boolean result = CommandPermission.handlePermissionPrediction(
-            "proxiesuite",
-            invocation
+        commandManager.register(
+            commandMeta,
+            createBrigadierCommand(plugin.getProxy())
         );
-
-        return result;
     }
 
-    @Override
-    public List<String> suggest(Invocation invocation) {
-        return I18N.handleSuggestion("proxiesuite", invocation);
+    private BrigadierCommand createBrigadierCommand(ProxyServer proxy) {
+        LiteralCommandNode<CommandSource> command =
+            BrigadierCommand.literalArgumentBuilder("proxie")
+                .then(reloadArgument())
+                .then(versionArgument())
+                .build();
+
+        return new BrigadierCommand(command);
+    }
+
+    private LiteralArgumentBuilder<CommandSource> versionArgument() {
+        LiteralArgumentBuilder<CommandSource> reloadArgument =
+            BrigadierCommand.literalArgumentBuilder("version").executes(ctx -> {
+                CommandSource source = ctx.getSource();
+
+                PluginContainer this_plugin = plugin
+                    .getProxy()
+                    .getPluginManager()
+                    .getPlugin("proxiesuite")
+                    .get();
+
+                String version = this_plugin
+                    .getDescription()
+                    .getVersion()
+                    .orElse("unknown");
+
+                I18N.sendMessage(
+                    source,
+                    "command.info.proxiesuite.version",
+                    Map.of("version", version)
+                );
+                return Command.SINGLE_SUCCESS;
+            });
+
+        return reloadArgument;
+    }
+
+    private LiteralArgumentBuilder<CommandSource> reloadArgument() {
+        LiteralArgumentBuilder<CommandSource> reloadArgument =
+            BrigadierCommand.literalArgumentBuilder("reload")
+                .requires(source -> {
+                    return source.hasPermission("proxiesuite.proxie.reload");
+                })
+                .executes(ctx -> {
+                    CommandSource source = ctx.getSource();
+
+                    try {
+                        boolean success = plugin.getConfig().reload();
+
+                        if (success) {
+                            I18N.sendMessage(
+                                source,
+                                "command.success.proxiesuite.reload"
+                            );
+
+                            return Command.SINGLE_SUCCESS;
+                        }
+                    } catch (IOException e) {
+                        plugin.getLogger().error("Unable to reload config!", e);
+                    }
+
+                    I18N.sendMessage(
+                        source,
+                        "command.error.proxiesuite.reload"
+                    );
+                    return Command.SINGLE_SUCCESS;
+                });
+
+        return reloadArgument;
     }
 }
